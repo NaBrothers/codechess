e4597ddcd6b63e3bc9a474c3d4fc792b507ad3be
navining <superokvv@gmail.com>
2021-08-11 01:06:51 +0800

    原子sequence

--- codechess-core/src/main/java/com/nabrothers/codechess/core/data/CodeObject.java
+++ codechess-core/src/main/java/com/nabrothers/codechess/core/data/CodeObject.java
@@ -2,19 +2,25 @@ package com.nabrothers.codechess.core.data;
 
 import lombok.Data;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Data
 public abstract class CodeObject {
+    private static AtomicInteger currentSeq = new AtomicInteger();
+
     protected int id;
+    protected int seq;
     protected int X;
     protected int Y;
     protected int type;
 
     protected CodeObject() {
-
+        this.seq = currentSeq.getAndIncrement();
     }
 
     protected CodeObject(int id, int type) {
         this.id = id;
         this.type = type;
+        this.seq = currentSeq.getAndIncrement();
     }
 }
