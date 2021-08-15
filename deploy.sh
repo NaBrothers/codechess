65ff8c4be068fbf5ebd1f4e2c54091e88b90e0f8
navining <superokvv@gmail.com>
2021-08-16 00:20:08 +0800

    部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "===== 开始发布 ====="
 echo "开始结束旧进程..."
-pid=`ps -ax | grep 'java -jar' | grep -v 'grep' | cut -d ' ' -f 2 | cut -d ' ' -f 1`
+pid=`ps -ax | grep 'java -jar' | grep -v 'grep' | cut -d ' ' -f 1`
 echo "旧进程结束成功！pid=[${pid}]"
 kill -9 $pid
 
@@ -13,7 +13,7 @@ echo "源代码编译成功！"
 echo "开始启动新进程..."
 cd codechess-core/target
 nohup java -jar codechess-core.jar &
-newPid=`ps -ax | grep 'java -jar' | grep -v 'grep' | cut -d ' ' -f 2 | cut -d ' ' -f 1`
+newPid=`ps -ax | grep 'java -jar' | grep -v 'grep' | cut -d ' ' -f 1`
 echo "新进程启动成功！pid=[${newPid}]"
 
 echo "===== 发布成功 ====="
