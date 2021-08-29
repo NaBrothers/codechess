a900ab33d4721adf580b5ea521b31c2f0ab854fd
navining <superokvv@gmail.com>
2021-08-30 03:08:38 +0800

    解决跨域问题

--- codechess-web/src/main/java/com/nabrothers/codechess/web/filter/CorsFilter.java
+++ codechess-web/src/main/java/com/nabrothers/codechess/web/filter/CorsFilter.java
@@ -1,6 +1,7 @@
 package com.nabrothers.codechess.web.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -10,9 +11,10 @@ public class CorsFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
             throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
         //本地debug跨域
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
