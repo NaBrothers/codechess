05094599ddc1a3c20b9e13c14fb10ced7ab74f13
navining <superokvv@gmail.com>
2021-08-31 01:07:08 +0800

    前端请求bug

--- codechess-frontend/globalUtils/globalUtils.js
+++ codechess-frontend/globalUtils/globalUtils.js
@@ -5,15 +5,6 @@ var loginUrl = "login/login.html";
 export {rootUrl, loginUrl};
 
 export function httpRequest(url, params, callback, type="GET") {
-    var token = localStorage.getItem('Authorization');
-    if (token == null){
-        var e = document.createEvent('MouseEvents'),
-        a = document.createElement('a');
-        a.href = loginUrl;
-        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-        a.dispatchEvent(e);
-        return;
-    }
     $.ajax({
         url: rootUrl+url,
         contentType: "application/json",
